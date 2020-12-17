3d1387659e003bcd9786f07ec75cdcbc2fb10bba
CervantesHuerta <cervanteshuerta@gmail.com>
2020-12-17 16:59:20 -0600

    commit de 17 de Diciembre de 2020

--- lib/clsMostrar.php
+++ lib/clsMostrar.php
@@ -1,4 +1,5 @@
-<?php require_once('sesion.php');
+<?php 
+//require_once('sesion.php');
 date_default_timezone_set('America/Mexico_City');
 require_once "clsModelo.php";
 
@@ -6,6 +7,12 @@ require_once "wialon.php";
 
 class mostrar extends Modelo
     {
-        
+        public function getTravelClass($id_cliente,$from,$to)
+        {
+            $qr = "SELECT * FROM tbl_rutas_activas WHERE fecha_fin between '$from' AND '$to' AND id_creador = $id_cliente";
+            
+            $result = $this->_db->query($qr);
+            return mysqli_fetch_assoc($result);
+        }
     }
 
--- lib/handler.php
+++ lib/handler.php
@@ -1,3 +1,56 @@
 <?php
-
+require_once('clsMostrar.php');
+$type = $_POST['type'];
+$ob = new mostrar();
+switch ($type) {
+    case 0:
+        # code...
+        break;
+    case 1:
+        # code...
+        break;
+    case 2:
+        # code...
+        break;
+    case 3:
+        # code...
+        break;
+    case 4:
+        # code...
+        break;
+    case 5:
+        # code...
+        break;
+    case 6:
+        # code...
+        break;
+    case 7:
+        # code...
+        break;
+    case 8:
+        # code...
+        break;
+    case 9:
+        # code...
+        break;
+    case 10:
+        # code...
+        break;
+    case 11:
+        # code...
+        break;
+    case 12:
+        # code...
+        break;
+    case 13:
+        # code...
+        break;
+    case 14:
+        # code...
+        break;
+    case 15:
+        # code...
+        break;
+    
+}
 ?>
