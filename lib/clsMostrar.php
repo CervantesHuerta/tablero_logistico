a7a21a553a24d1b25a943f28dc6b49911b7faadf
CervantesHuerta <cervanteshuerta@gmail.com>
2020-12-23 18:05:59 -0600

    commit del 23 de Diciembre, falta clasificar las unidades en la funcion de getUnidadesData

--- lib/clsMostrar.php
+++ lib/clsMostrar.php
@@ -12,11 +12,62 @@ class mostrar extends Modelo
             $qr = "SELECT * FROM tbl_rutas_activas WHERE fecha_fin between '$from' AND '$to' AND id_creador = $id_cliente";
             //die($qr);
             $a=[];
+            $d=[];
+            $i=0;
+            $j=0;
+            $k=0;
+            $l=0;
             $result = $this->_conn->query($qr);
             while ($row = mysqli_fetch_assoc($result)) {
-                $a[]=$row;
+                $id_act=$row['id_rutas_activas'];
+                $qr2 ="SELECT * FROM tbl_valores_reales WHERE id_ruta = $id_act";
+                $r = $this->_conn->query($qr2);
+                //die(json_encode($rr));
+                while ($rr = mysqli_fetch_assoc($r)) {
+
+                    if (strtotime($rr['hora_real'])<strtotime($row['fecha_fin'])) {
+                        
+                        $a['a_tiempo'][$j]['esperados']=$row;
+                        $a['a_tiempo'][$j]['reales']= $rr;
+                        $j++;
+                    }
+                    elseif (strtotime($rr['hora_real'])>strtotime($row['fecha_fin'])) {
+                        $a['retrasados'][$i]['esperados']=$row;
+                        $a['retrasados'][$i]['reales']= $rr;
+                        $i++;
+                    }
+                   
+                    
+                }
+                if ($row['id_estatus']==1 && strtotime($row['fecha_inicio'])> time() ) {
+                    $a['en_espera'][$k]['esperados']=$row;
+                    $k++;
+                }elseif ($row['id_estatus']==1 && strtotime($row['fecha_inicio']) < time()) {
+                    $a['en_curso'][$l]['esperados']=$row;
+                    $l++;
+                }
+                $a['c_a_tiempo']=count($a['a_tiempo']);
+                $a['c_retrasados']=count($a['retrasados']);
+                $a['c_en_curso']=count($a['en_curso']);
             }
             return $a;
+        }
+        public function getUnitsData($id_cliente)
+        {
+          $qr = "SELECT * FROM tbl_clienteplataforma WHERE id_login = $id_cliente";
+          //die($qr);
+          $result = $this->_db->query($qr);
+         $a=[];
+         while ($r =  mysqli_fetch_assoc($result)) {
+             $id=$r['id_cliente'];
+            $qrr = "SELECT * FROM tbl_unidades WHERE id_cliente =$id ;";
+            $rt = $this->_db->query($qrr);
+            while ($rr =mysqli_fetch_assoc($rt)) {
+                $a[]=$rr;
+                //falta clasificar las unidades de acuerdo al status que tienen.
+            }
+         }
+         return $a;
         }
 
        /*  public function save_response($response)
--- lib/test.php
+++ lib/test.php
@@ -1,18 +1,14 @@
 <?php
    require_once('clsMostrar.php');
    $cl = new mostrar();
-<<<<<<< HEAD
-   $travels = $cl->getTravelClass(10142,'2019-01-14 07:25:11','2020-12-15 07:25:11');
-=======
-   $travels = $cl->getTravelClass(10142,'2020-10-14 07:25:11','2020-12-15 07:25:11');
->>>>>>> 92d8ca80cfdef9fe4a895c92ecf1a36ec539cb38
-    echo json_encode($travels);
+   
+   /* $travels = $cl->getTravelClass(10188,'2019-01-14 07:25:11','2020-12-23 07:25:11');
+   echo json_encode($travels); */
 
     /* $json = '[{"id":"1","nombre":"Jose","edad":"25","genero":"masculino","email":"josegonzales9871@gmail.com","localidad":"Madrid","telefono":"912546524"},{"id":"2","nombre":"Juan","edad":"31","genero":"masculino","email":"juanrodriguez65465@gmail.com","localidad":"Barcelona","telefono":"934654654"},{"id":"3","nombre":"Antonio","edad":"43","genero":"masculino","email":"antoni654654@gmail.com","localidad":"Valencia","telefono":"214748366"},{"id":"4","nombre":"Angelina","edad":"35","genero":"femenino","email":"angelina654456@gmail.com","localidad":"New York","telefono":"247483647"}]';
-
-
-    
-
     $cl->save_response($json); */
+
+    $units = $cl->getUnitsData(10188);
+    echo json_encode($units);
 
 ?>
